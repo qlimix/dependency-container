caa55e6ec65d666ba93f38dd1c2dff56c884a928
frank <blixia@qlimix.net>
2018-02-24 13:49:13 +0100

    provider throws exception

--- src/DependencyProviderInterface.php
+++ src/DependencyProviderInterface.php
@@ -2,10 +2,14 @@
 
 namespace Qlimix\DependencyContainer;
 
+use Qlimix\DependencyContainer\Exception\DependencyException;
+
 interface DependencyProviderInterface
 {
     /**
      * @param DependencyMergerInterface $merger
+     *
+     * @throws DependencyException
      */
     public function provide(DependencyMergerInterface $merger): void;
 }
