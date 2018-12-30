23333fcb07bae5f4efe5965fc39274242086d235
frank <frank@qlimix.net>
2018-12-30 16:35:43 +0100

    added make function

--- src/DependencyRegistryInterface.php
+++ src/DependencyRegistryInterface.php
@@ -6,15 +6,15 @@ use Psr\Container\ContainerInterface;
 
 interface DependencyRegistryInterface extends ContainerInterface
 {
-    /**
-     * @param string $id
-     * @param callable $service
-     */
     public function set(string $id, callable $service): void;
 
     /**
-     * @param string $id
      * @param mixed $value
      */
     public function setValue(string $id, $value): void;
+
+    /**
+     * @return mixed
+     */
+    public function make(string $id);
 }
